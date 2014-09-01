bcd5dd1e75db587bb7d520e81d9c698adecb4942
codyroot <smithyyy@myopera.com>
2014-09-01 21:37:17 +0200

    Init

---
+++ yo.js
@@ -0,0 +1,77 @@
+var myFunc = (function yo() {
+    var a = "my Var";
+    var b = [1, 2, 3];
+    var c = {
+        a: 1,
+        b: 2
+    }
+    var d = function () {
+    };
+
+
+    return {
+        d: d
+    }
+}());
+
+
+var myObj = {
+    prop: "my Prop",
+    fn: function () {
+    }
+};
+
+function fn() {
+    var e = "my E";
+
+    function a() {
+    };
+    return e;
+};
+
+var es5 = Object.create(null, {
+    prop: {
+        value: "10"
+    },
+    getterSetter: {
+        get: function () {
+        },
+        set: function () {
+        }
+    },
+    method: {
+        value: function () {
+        }
+    }
+});
+
+var Klass = function () {
+    this.prop = "my prop";
+};
+
+Klass.prototype.aha = function () {
+
+};
+
+Object.defineProperties(Klass.prototype, {
+    "prop1": {
+        value: "10"
+    },
+    "getterSetter1": {
+        get: function () {
+        },
+        set: function () {
+        }
+    },
+    "method1": {
+        value: function () {
+        }
+    }
+});
+
+Object.defineProperty(Klass.prototype, "blubb", {
+    value: "asd"
+});
+
+var klass = new Klass();
+console.dir(klass);
